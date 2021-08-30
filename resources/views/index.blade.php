f525fd100e07bddef49af2685c69109e1b97fe4d
Pravin Bambale <astuteconstrotech@gmail.com>
2021-08-30 23:50:30 +0530

    banner setup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,13 +3,13 @@
   <head>
     <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>BCMS Cloud | Responsive Bootstrap 5 Admin Dashboard Template</title>
+      <title>BCMS Cloud</title>
       
       <!-- Favicon -->
       <link rel="shortcut icon" href="{{asset('/assets/images/favicon.ico')}}" />
       
       <!-- Library / Plugin Css Build -->
-      <link rel="stylesheet" href="{{asset('/assets/css/libs.min.css')}}">
+      <!-- <link rel="stylesheet" href="{{asset('/assets/css/libs.min.css')}}"> -->
       
       <!-- Custom Css -->
       <link rel="stylesheet" href="{{asset('/assets/css/hope-ui.css?v=1.0.2')}}">  </head>
@@ -38,50 +38,16 @@
                 <h4 class="text-white mb-5">Production ready FREE Open Source <b>Dashboard UI Kit</b> and <b>Design System</b>.</h4>
                 <div class="d-flex justify-content-center align-items-center">
                     <div>
-                        <a class="btn btn-light bg-white" target="_blank" href="{{url('/dashboard/')}}">Dashboard Demo</a>
+                        <a class="btn btn-light bg-white" target="_blank" href="{{url('/register')}}">Register</a>
                     </div>
                     <div class="ms-3">
-                        <a class="btn btn-light bg-white" href="#components">UI KIT</a>
+                        <a class="btn btn-light bg-white" href="#components">Get Call</a>
                     </div>
                 </div>
 
             </div>
         </div>
-        <div class="container">
-            <nav class="nav navbar navbar-expand-lg navbar-light top-1 rounded">
-                <div class="container-fluid">
-                    <a class="navbar-brand mx-2" href="#">
-                        <!--Logo start-->
-                        <svg width="30" class="text-primary" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <rect x="-0.757324" y="19.2427" width="28" height="4" rx="2" transform="rotate(-45 -0.757324 19.2427)" fill="currentColor"/>
-                            <rect x="7.72803" y="27.728" width="28" height="4" rx="2" transform="rotate(-45 7.72803 27.728)" fill="currentColor"/>
-                            <rect x="10.5366" y="16.3945" width="16" height="4" rx="2" transform="rotate(45 10.5366 16.3945)" fill="currentColor"/>
-                            <rect x="10.5562" y="-0.556152" width="28" height="4" rx="2" transform="rotate(45 10.5562 -0.556152)" fill="currentColor"/>
-                        </svg>
-                        <!--logo End-->                        <h5 class="logo-title">BCMS Cloud</h5>
-                    </a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-2" aria-controls="navbar-2" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbar-2">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-start">
-                            <li class="nav-item me-3">
-                                <a class="nav-link" aria-current="page" href="https://templates.iqonic.design/hope-ui/documentation/html/dist/main/" target="_blank">Documentation</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="btn btn-primary" aria-current="page" href="https://iqonic.design/product/admin-templates/hope-ui-admin-free-open-source-bootstrap-admin-template/" target="_blank">
-                                    <svg width="22" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path opacity="0.4" fill-rule="evenodd" clip-rule="evenodd" ></path>
-                                        <path fill-rule="evenodd" clip-rule="evenodd" class="bg-soft-primary"></path>
-                                    </svg>
-                                    Sign Up
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
+        @include('nav')
     </div>
    
    <!--  <article id="list-group">
---
+++ resources/views/nav.blade.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <nav class="nav navbar navbar-expand-lg navbar-light top-1 rounded">
+        <div class="container-fluid">
+            <a class="navbar-brand mx-2" href="#">
+                <!--Logo start-->
+                <svg width="30" class="text-primary" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <rect x="-0.757324" y="19.2427" width="28" height="4" rx="2" transform="rotate(-45 -0.757324 19.2427)" fill="currentColor"/>
+                    <rect x="7.72803" y="27.728" width="28" height="4" rx="2" transform="rotate(-45 7.72803 27.728)" fill="currentColor"/>
+                    <rect x="10.5366" y="16.3945" width="16" height="4" rx="2" transform="rotate(45 10.5366 16.3945)" fill="currentColor"/>
+                    <rect x="10.5562" y="-0.556152" width="28" height="4" rx="2" transform="rotate(45 10.5562 -0.556152)" fill="currentColor"/>
+                </svg>
+                <!--logo End--> 
+                <h5 class="logo-title">BCMS Cloud</h5>
+            </a>
+            <div class="collapse navbar-collapse" id="navbar-2">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex align-items-start">
+                    <li class="nav-item me-3">
+                        <a class="nav-link" aria-current="page" href="{{url('/')}}" >Home</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" aria-current="page" href="{{url('/modules')}}" >Modules</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" aria-current="page" href="{{url('/about-us')}}" >About us</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" aria-current="page" href="{{url('/about-us')}}" >Contact</a>
+                    </li>
+                    <li class="nav-item me-3">
+                        <a class="nav-link" aria-current="page" href="{{url('/register')}}" target="_blank">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</div>
